380e7b123a29fd3c4774417595868fc0d62852de
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-11-25 09:47:20 +0200

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Asaf Baibekov on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
