9334e60e851e7b5250931c02d48568432d9396ce
Asaf Baibekov <asaf.baibekov@stu.shenkar.ac.il>
2022-12-06 11:29:32 +0200

    make the properties of the card published to update whoever listen for changes in the card model

--- MemoryGame/Card/Card.swift
+++ MemoryGame/Card/Card.swift
@@ -10,9 +10,9 @@ import SwiftUI
 class Card<Content>: Identifiable, ObservableObject {
 	var id: Int = 0
 
-	var isFaceUp: Bool
-	var isMatched: Bool
-	var content: Content
+	@Published var isFaceUp: Bool
+	@Published var isMatched: Bool
+	@Published var content: Content
 	
 	init(id: Int, isFaceUp: Bool = false, isMatched: Bool = false, content: Content) {
 		self.id = id
